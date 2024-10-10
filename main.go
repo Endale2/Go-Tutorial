0907c473da42803987e553b1a7f65fa5c386c959
Endale2 <endale406@gmail.com>
2024-10-10 10:00:36 +0300

    conditional statements

--- main.go
+++ main.go
@@ -3,7 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
+	day := "Tuesday"
+
+	switch day {
+	case "Monday":
+		fmt.Println("It's the start of the week.")
+	case "Tuesday":
+		fmt.Println("It's Tuesday!")
+	case "Friday":
+		fmt.Println("It's almost the weekend!")
+	default:
+		fmt.Println("It's a regular day.")
 	}
 }
