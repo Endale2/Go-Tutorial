1038fbb1df0096e11b22d3d944be42b1180834b5
Endale2 <endale406@gmail.com>
2024-11-04 22:56:35 +0300

    revision on functions

--- main.go
+++ main.go
@@ -4,19 +4,38 @@ import (
 	"fmt"
 )
 
-func main() {
-
-	r1 := Rectangle{3, 4}
-
-	fmt.Printf("The area of the rectangle is : %v ", r1.Area())
-
-}
-
 type Rectangle struct {
 	width  int
 	height int
 }
 
+// Value receiver: This method will not modify the original Rectangle
+func (r Rectangle) ScaleBy(factor int) {
+	r.width *= factor
+	r.height *= factor
+}
+
+// Pointer receiver: This method will modify the original Rectangle
+func (r *Rectangle) ScaleByPointer(factor int) {
+	r.width *= factor
+	r.height *= factor
+}
+
+// Area method for calculating area (same for both receivers)
 func (r Rectangle) Area() int {
-	return r.height * r.width
+	return r.width * r.height
+}
+
+func main() {
+	// Create a Rectangle instance
+	r1 := Rectangle{width: 3, height: 4}
+
+	// Using value receiver
+	fmt.Println("Original Rectangle:", r1)
+	r1.ScaleBy(2)
+	fmt.Println("After ScaleBy (value receiver):", r1) // No change in width and height
+
+	// Using pointer receiver
+	r1.ScaleByPointer(2)
+	fmt.Println("After ScaleByPointer (pointer receiver):", r1) // Changes in width and height
 }
